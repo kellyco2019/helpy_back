1f339649dcc19fb26057314c7acfbc93ade537a4
kellyco2019 <48420443+kellyco2019@users.noreply.github.com>
2021-08-22 17:59:59 -0500

    sse agregan variables de entorno (#14)

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const connect = require("./db");
 const studentRouter = require("./routes/student");
 const teacherRouter = require("./routes/teacher");
 const lessonsRouter = require("./routes/lessons");
-//const {verify} = require("./utils/mailer")
+
 
 const port = process.env.PORT || 8000;
 const app = express();
@@ -14,8 +14,10 @@ connect();
 //verify()
 
 app.use(express.json());
-app.use(cors());
-//{origin: process.env.FRONTEND_URL || 'http://localhost:3000'}
+app.use(cors(
+//{origin: process.env.FRONTEND_URL || 'http://192.168.20.21:8000'}  
+));
+
    
 app.use(morgan("dev"));
 
